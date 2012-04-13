dfd91ea0e575fe5f3639a7606def1fbb60dc5324
Roger Graves <rogergraves@gmail.com>
2012-04-13 10:33:56 -0700

    valid presence of name on Sandwich w/ test

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,2 +1,3 @@
 class Sandwich < ActiveRecord::Base
+  validates :name, :presence => true
 end
--- spec/models/sandwich_spec.rb
+++ spec/models/sandwich_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Sandwich do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "create" do
+    it "requires a name" do
+      sandwich = Sandwich.new
+      sandwich.valid?
+      sandwich.errors[:name].should_not be_empty
+    end
+  end
 end
