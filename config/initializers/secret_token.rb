1644fff9fd94eb71421ac212edb9ac9eda635b19
Roger Graves <rogergraves@gmail.com>
2012-04-13 09:38:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandwichorder::Application.config.secret_token = 'f622635e1a670496787636f1e67f6cb10fbc3c10933f9a90ba116b9749315f88147012d4c9b73c7fd5dab4f9f99bcf0b7ca06beeaa8d90915aefe99cf7a15aaa'
